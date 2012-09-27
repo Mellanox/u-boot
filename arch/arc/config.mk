24c43305ee9f47983038e277a6731edf81f47fb7
Frank Dols <frank.dols@synopsys.com>
2012-09-27 14:27:17 +0200

    ARC [snsp-haps51] introduce ARC to u-boot
    Signed-off-by: Frank Dols <frank.dols@synopsys.com>

---
+++ arch/arc/config.mk
@@ -0,0 +1,15 @@
+#
+# Copyright (C) 2012 Synopsys, Inc. (www.synopsys.com)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+
+CROSS_COMPILE ?= arc-elf32-
+
+PLATFORM_CPPFLAGS+= -D__ARC__ -DCONFIG_ARC
+
+CONFIG_STANDALONE_LOAD_ADDR = 0xc100000
+
+LDSCRIPT := $(SRCTREE)/$(CPUDIR)/u-boot.lds
